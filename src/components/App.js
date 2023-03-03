bcee384dadeb911a85ad987214a1eece095ac904
Andrew Sanchez <druu831@gmail.com>
2023-03-03 14:47:49 -0800

    typography styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>The Octocat</h1>
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <h3>@Octocat</h3>
+      <h4>Repos</h4>
+      <p>Im a paragraph</p>
+    </div>
+  );
 }
